f057bf7146597d49856db3ed8504347f4ba15867
Sarthak Kumar <realsarthakkumar@gmail.com>
2025-08-11 15:02:42 +0530

    Add authentication with GitHub using NextAuth and update project metadata

---
+++ src/auth.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    providers: [
+        GitHub({
+            clientId: process.env.AUTH_GITHUB_ID,
+            clientSecret: process.env.AUTH_GITHUB_SECRET,
+            authorization: {
+                params: {
+                    scope: "repo read:user user:email",
+                },
+            },
+        }),
+    ],
+    secret: process.env.AUTH_SECRET,
+})
